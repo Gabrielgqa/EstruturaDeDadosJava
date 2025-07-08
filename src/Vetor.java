242c1c0766d9589c2e3078985747a8d1fe9f7356
GabrielGQA <gabrielgqa@gmail.com>
2025-07-07 22:12:22 -0300

    feat: adiciona no vetor refatorado

--- src/Vetor.java
+++ src/Vetor.java
@@ -1,17 +1,24 @@
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
     }
 
-    public void adiciona(String elemento){
+    /*public void adiciona(String elemento){
         for (int i = 0; i < elementos.length; i++){
             if(elementos[i] == null){
                 elementos[i] = elemento;
                 break;
             }
         }
+    }*/
+
+    public void adiciona(String elemento){
+        elementos[tamanho] = elemento;
+        tamanho++;
     }
 }
